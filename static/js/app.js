f0da4b6ec77da12f7925c1e94b5099aad0bd04cf
annawiste <12013474+annawiste@users.noreply.github.com>
2021-11-09 15:39:52 -0500

    buildTable function

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear existing data
+    tbody.html("");
+    // loop through the array data
+    data.forEach((dataRow) => {
+        //Append a row to the table body
+        let row = tbody.append("tr");
+        //loop through each field and add the value as a cell in table
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+
+}
